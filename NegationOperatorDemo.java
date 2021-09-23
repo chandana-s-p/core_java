934058d0e6e66b4e053a2fc2ef6c045b6eff2137
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-23 19:25:52 +0530

    writes negation operator program

---
+++ NegationOperatorDemo.java
@@ -0,0 +1,8 @@
+class NegationOperatorDemo{
+public static void main(String[] args){
+    boolean b=true;
+    boolean result=!b;
+System.out.println(" b : "+b);
+System.out.println(" result : "+result);
+}
+}
