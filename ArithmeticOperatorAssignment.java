67a04b8f8f44b074690ee11f4c57be3c980e5769
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-23 20:00:38 +0530

    writes arithmetic operator assignment program

---
+++ ArithmeticOperatorAssignment.java
@@ -0,0 +1,29 @@
+class ArithmeticOperatorAssignment{
+public static void main(String[] args){
+    int m=65;
+    int n=50;
+    int addResult=m+n;
+System.out.println("addResult :"+addResult);
+
+    int i=39;
+    int j=20;
+    int subResult=i-j;
+System.out.println("subResult :"+subResult);
+
+    int a=15;
+    int b=9;
+    int mulResult=a*b;
+System.out.println("mulResult :"+mulResult);
+
+    int x=10;
+    int y=100;
+    int divResult=y/x;
+System.out.println("divResult :"+divResult);
+
+    int c=19;
+    int d=2;
+    int modResult=c%d;
+System.out.println("modResult :"+modResult);
+
+   }
+ }
