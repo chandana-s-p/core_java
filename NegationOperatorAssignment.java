cd8ad20830b4b7334c8f30c2de05cfd12acedeaa
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-23 19:40:58 +0530

    writes negation operator assignment program

---
+++ NegationOperatorAssignment.java
@@ -0,0 +1,8 @@
+class NegationOperatorAssignment{
+public static void main(String[] args){
+    boolean b=false;
+    boolean result=!b;
+System.out.println(" b : "+b);
+System.out.println(" result : "+result);
+}
+}
