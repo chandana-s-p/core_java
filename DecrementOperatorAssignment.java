0d2bbd90769dff71337f80cee3334b1df40b43d1
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-23 19:38:00 +0530

    writes decrement operator assignment program

---
+++ DecrementOperatorAssignment.java
@@ -0,0 +1,15 @@
+class DecrementOperatorAssignment{
+public static void main(String[] args){
+System.out.println("Pre Decrement Operation.......");
+   int m=15;
+   int n=--m;
+System.out.println(" m :"+m);
+System.out.println(" n :"+n);
+
+System.out.println("Post Decrement Operation.......");
+    int i=25;
+    int j=i--;
+System.out.println(" i :"+i);
+System.out.println(" j :"+j);
+}
+}
