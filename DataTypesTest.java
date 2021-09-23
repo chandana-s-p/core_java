abecaa2deeb552dd470336fef6063e7baf644f0d
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-23 19:12:31 +0530

    writes datatypestest program

---
+++ DataTypesTest.java
@@ -0,0 +1,28 @@
+class DataTypesTest{
+   public static void main(String[] args){
+        byte b=10;
+    System.out.println(" b : "+b);
+
+    short s=30000;
+System.out.println(" s : "+s);
+
+    int i=100000;
+System.out.println(" i : "+i);
+
+    long l=100000000000l;
+System.out.println(" l : "+l);
+
+    float f=20.00f;
+System.out.println(" f : "+f);
+
+    double d=20.40;
+System.out.println(" d : "+d);
+
+    char c='$';
+System.out.println(" c : "+c);
+
+    boolean bo=false;
+System.out.println(" bo : "+bo);
+}
+}
+    
