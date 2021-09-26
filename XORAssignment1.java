a2f36a9f2f9f1b4a3afa8fa5e082084e6f440db7
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 22:07:39 +0530

    writes the xor operator assignment1 program

---
+++ XORAssignment1.java
@@ -0,0 +1,8 @@
+class XORAssignment1{
+public static void main(String[] args){
+   int x=5;
+   int y=7;
+System.out.println("x>y ^ y!=x : "+(x>y ^ y!=x));
+System.out.println("x>y ^ y<x : "+(x>y ^ y<x));
+}
+}
