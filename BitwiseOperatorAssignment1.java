1ced7f40438d1c326faaf1b71685b9b80fbab8e1
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 22:06:09 +0530

    writes the bitwise operator assignment1 program

---
+++ BitwiseOperatorAssignment1.java
@@ -0,0 +1,8 @@
+class BitwiseOperatorAssignment1{
+public static void main(String[] args){
+   int x=9;
+   int y=8;
+System.out.println(" x<y & x++ !=y : "+( x<y & x++ !=y ));
+System.out.println(" x>=y | x<= ++y : "+(x>=y | x<= ++y));
+}
+}
