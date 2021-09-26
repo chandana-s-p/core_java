d69b5441ecc99d73ede7d25ab69cf3382711b93c
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 13:51:59 +0530

    writes the unary operator assignment4 program

---
+++ UnaryOperatorAssignment4.java
@@ -0,0 +1,10 @@
+class UnaryOperatorAssignment4{
+public static void main(String[] args){
+  int i=4;
+  int j=21;
+  int k=++i;
+  j--;
+System.out.println(" k : "+k);
+System.out.println("j : "+j);
+}
+}
