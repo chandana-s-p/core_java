faf929f8e5e7f621e88f87c5c7cdc353be344a51
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 21:46:49 +0530

    writes the relational operator assignment2 program

---
+++ RelationalOperatorAssignment2.java
@@ -0,0 +1,12 @@
+class RelationalOperatorAssignment2{
+public static void main(String[] args){
+    int var1=9;
+    int var2=12;
+System.out.println(" var1!=var2 : "+(var1!=var2)); 
+System.out.println(" var1>var2 : "+(var1>var2)); 
+System.out.println(" var1<=var2 : "+(var1<=var2)); 
+System.out.println(" var1>=var2 : "+(var1>=var2)); 
+System.out.println(" var1<var2 : "+(var1<var2)); 
+System.out.println(" var1==var2 : "+(var1==var2));
+}
+} 
