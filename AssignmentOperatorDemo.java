bf0c81c2c7972e375f0e399ed83152d3612dc553
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 21:39:25 +0530

    writes the assignment operator demo program

---
+++ AssignmentOperatorDemo.java
@@ -0,0 +1,20 @@
+class AssignmentOperatorDemo{
+public static void main(String[] args){
+   int m=50;
+   int n=10;
+   n+=m;
+System.out.println(" n : "+n);
+   int i=50;
+   int j=10;
+   i/=j;
+System.out.println(" i : "+i);
+   int a=50;
+   int b=10;
+   a-=b;
+System.out.println(" a : "+a);
+   int x=50;
+   int y=10;
+   x*=y;
+System.out.println(" x : "+x);
+}
+}
