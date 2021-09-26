09a29c87951bcf58ee81905261d2b16129bfaf08
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 21:41:30 +0530

    writes the assignment operator assignment1 program

---
+++ AssignmentOperatorAssignment1.java
@@ -0,0 +1,13 @@
+class AssignmentOperatorAssignment2{
+public static void main(String[] args){
+   int a=2;
+   int b=2;
+   int c=2;
+   a+=b;
+   b-=c;
+   c/=(a+b);
+System.out.println(" a : "+a);
+System.out.println(" b : "+b);
+System.out.println(" c : "+c);
+}
+}
