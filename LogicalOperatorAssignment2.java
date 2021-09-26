e0519b7ee7976623ee31559f99ef6b91c1ff818e
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 22:00:49 +0530

    writes the logical operator assignment2 program

---
+++ LogicalOperatorAssignment2.java
@@ -0,0 +1,10 @@
+class LogicalOperatorAssignment2{
+public static void main(String[] args){
+  int var1=10;
+  int var2=20;
+  int var3=20;
+System.out.println(var1<var2 && var2==var3);
+System.out.println(var1>var2 && var2!=var3);
+
+}
+}
