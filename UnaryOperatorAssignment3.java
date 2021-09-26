5295e10c5fbb77de52cbca9bcc86359d76fde840
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 13:51:16 +0530

    writes the unary operator assignment3 program

---
+++ UnaryOperatorAssignment3.java
@@ -0,0 +1,14 @@
+class UnaryOperatorAssignment3{
+public static void main(String[] args){
+ int a=10;
+ int b=5;
+ int c;
+ int d;
+ c = ++b;
+ d = a++;
+System.out.println("a : "+a);
+System.out.println("b : "+b);
+System.out.println("c : "+c);
+System.out.println("d : "+d);
+}
+}
