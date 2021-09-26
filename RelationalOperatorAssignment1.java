de31671d6a28b85c8cec1816332a820201cfd156
Chandana-S-Patil <chandanasp8@gmail.com>
2021-09-26 21:46:22 +0530

    writes the relational operator assignment1 program

---
+++ RelationalOperatorAssignment1.java
@@ -0,0 +1,18 @@
+class RelationalOperatorAssignment1{
+public static void main(String[] args){ 
+   int a=50;
+   int b=100;
+System.out.println("a==b : "+(a==b));
+System.out.println("a!=b : "+(a!=b));
+System.out.println("a>b : "+(a>b));
+System.out.println("a<b : "+(a<b));
+System.out.println("a>=b : "+(a>=b));
+System.out.println("a<=b : "+(a<=b));
+}
+}
+
+
+   
+   
+
+  
